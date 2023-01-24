b1e508c58bd8c5e11bd410227b00e97748957437
HeleneAmouzou <helene.amouzou@gmail.com>
2023-01-24 11:24:03 +0100

    Suite de Fibonacci version finale

--- PhpstormProjects/SuiteDeFibonacci/fibonacci.php
+++ PhpstormProjects/SuiteDeFibonacci/fibonacci.php
@@ -1,21 +1,19 @@
 <?php
-$compt=0;
-$nbprecedentun=0;
-$nbprecedentdeux=0;
-$nbactuel=1;
+$compt = 0;
+$nbprecedentun = 1;
+$nbprecedentdeux = 0;
+$nbactuel = 1;
 
-while ($nbactuel<100){
-    $nbactuel=$nbprecedentun+$nbprecedentdeux;
-    if ($nbactuel%2==0){
-        $compt=$compt+$nbactuel;
-        echo $compt;
+while ($nbactuel < 4000000) { //on considère que l’élément le plus grand est strictement inférieur à 4 millions.
+    $nbactuel = $nbprecedentun + $nbprecedentdeux; //Chaque élément est constitué de la somme des deux éléments précédents
+
+    if ($nbactuel % 2 === 0) { //Détermine si élement pair, les conserve et additionne
+        $compt = $compt + $nbactuel;
+        //echo $compt;
     }
-    $nbprecedentdeux=$nbprecedentun;
-    $nbprecedentun=$nbactuel;
-
+    $nbprecedentdeux = $nbprecedentun;
+    $nbprecedentun = $nbactuel;
 }
 
-echo '<p>Somme des éléments pairs = ' . $compt . '</p>';
+echo '<p>Somme des éléments pairs = ' . $compt . '</p>'; //Affiche : Somme des éléments pairs = 4613732
 
-
-?>
